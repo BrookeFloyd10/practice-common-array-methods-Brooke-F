7fe137d9b650aa4b06c21f0d37387f118123fbf2
Brooke F <brooke.floyd10@gmail.com>
2026-04-29 19:10:11 -0500

    added clairification for task 4

---
+++ common-array-methods.js
@@ -0,0 +1,41 @@
+
+// Task 1
+const drinkOrders = ["Latte", "Tea", "Espresso"];
+const pastryOrders = ["Croissant", "Danish", "Bagle"];
+
+
+// Task 2
+console.log(`
+Total Drink Orders: ${drinkOrders.length}
+Total Pastry Orders: ${pastryOrders.length}
+ `);
+
+// Task 3
+console.log(`
+First drink item:  ${drinkOrders[2]} and Second pastry item: ${pastryOrders[1]}, 
+
+Second drink item: ${drinkOrders[0]} and Third pastry item: ${pastryOrders[2]},
+
+Second drink item:  ${drinkOrders[1]} and First pastry item: ${pastryOrders[0]}
+`);
+
+
+// Task 4 
+ let drinkSelection = 1;
+ let pastrySelection = 0;
+    console.log(`You Selected: ${drinkOrders[drinkSelection]} and ${pastryOrders[pastrySelection]}`)
+
+
+// Task 5
+console.log("Current Orders:");
+for (let i = 0; i < drinkOrders.length; i++){
+    console.log(drinkOrders[i])
+}
+
+// Task 6 
+
+drinkOrders.push("Flat White");
+console.log(`
+Current number of drink orders: ${(drinkOrders.length)} 
+Current drink list: ${(drinkOrders)}
+`);
